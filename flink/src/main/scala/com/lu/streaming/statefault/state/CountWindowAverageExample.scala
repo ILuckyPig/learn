70a730050ea274530dab4c9fa35fac53517e3eb9
ILuckyPig <980534016.qq.com>
2019-10-01 23:06:21 +0800

    use keyed state

---
+++ flink/src/main/scala/com/lu/streaming/statefault/state/CountWindowAverageExample.scala
@@ -0,0 +1,17 @@
+package com.lu.streaming.statefault.state
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object CountWindowAverageExample {
+  def main(args: Array[String]): Unit = {
+    val environment = StreamExecutionEnvironment.getExecutionEnvironment
+    val source = environment.socketTextStream("localhost", 9999, '\n')
+      source
+      .map(line => (line.split(",")(0).toLong, line.split(",")(1).toLong))
+      .keyBy(0)
+      .flatMap(new CountWindowAverage())
+      .print()
+    environment.execute()
+  }
+}
