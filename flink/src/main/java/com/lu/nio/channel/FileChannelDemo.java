b747b9c563bea795929417d03424385a599808b1
luckypig <980534016@qq.com>
2020-05-25 20:01:42 +0800

    flink network nio - file channel demo

---
+++ flink/src/main/java/com/lu/nio/channel/FileChannelDemo.java
@@ -0,0 +1,34 @@
+package com.lu.nio.channel;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * flink network nio
+ */
+public class FileChannelDemo {
+    public static void main(String[] args) throws IOException {
+        URL resource = FileChannelDemo.class.getResource("/");
+        RandomAccessFile aFile = new RandomAccessFile(resource.getPath() + "log4j.properties", "rw");
+        FileChannel inChannel = aFile.getChannel();
+
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        int bytesRead = inChannel.read(buf);
+        while (bytesRead != -1) {
+            buf.flip();
+
+            while (buf.hasRemaining()) {
+                System.out.print((char) buf.get());
+            }
+
+            buf.clear();
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
+    }
+}
