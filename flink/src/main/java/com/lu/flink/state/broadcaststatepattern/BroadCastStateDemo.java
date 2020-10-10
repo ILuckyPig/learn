6d0593629758ddee0a4b2bd578f34d9bf11b0dea
luckypig <980534016@qq.com>
2020-10-10 20:01:38 +0800

    add BroadCastStateDemo

--- flink/src/main/java/com/lu/flink/state/broadcaststatepattern/BroadCastStateDemo.java
+++ flink/src/main/java/com/lu/flink/state/broadcaststatepattern/BroadCastStateDemo.java
@@ -20,64 +20,70 @@ import java.util.Map;
 
 public class BroadCastStateDemo {
     public static void main(String[] args) throws Exception {
-        // TODO can output
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStream<Tuple2<Integer, String>> source1 = environment.fromElements(Tuple2.of(1, "a"), Tuple2.of(2, "b"), Tuple2.of(3, "c"));
-        DataStream<Tuple2<Integer, String>> source2 = environment.fromElements(Tuple2.of(1, "张三"), Tuple2.of(2, "李四"));
+        DataStream<Tuple2<Integer, String>> source1 = environment
+                .socketTextStream("localhost", 9999)
+                .map(message -> {
+                    String[] strings = message.split(",");
+                    return Tuple2.of(Integer.parseInt(strings[0]), strings[1]);
+                }).returns(Types.TUPLE(Types.INT, Types.STRING));
+        DataStream<Tuple2<Integer, String>> broadcastSource = environment
+                .socketTextStream("localhost", 9998)
+                .map(message -> {
+                    String[] strings = message.split(",");
+                    return Tuple2.of(Integer.parseInt(strings[0]), strings[1]);
+                }).returns(Types.TUPLE(Types.INT, Types.STRING));
         KeyedStream<Tuple2<Integer, String>, Integer> keyedStream = source1.keyBy(tuple2 -> tuple2.f0);
 
         MapStateDescriptor<Integer, Tuple2<Integer, String>> mapStateDescriptor = new MapStateDescriptor<>(
                 "BroadCastState", Types.INT, TypeInformation.of(new TypeHint<Tuple2<Integer, String>>() {}));
-        BroadcastStream<Tuple2<Integer, String>> broadcastStream = source2.broadcast(mapStateDescriptor);
+        BroadcastStream<Tuple2<Integer, String>> broadcastStream = broadcastSource.broadcast(mapStateDescriptor);
 
         keyedStream.connect(broadcastStream)
                 .process(new KeyedBroadcastProcessFunction<Integer, Tuple2<Integer, String>, Tuple2<Integer, String>, String>() {
-                    // store partial matches, i.e. first elements of the pair waiting for their second element
-                    // we keep a list as we may have many first elements waiting
                     private final MapStateDescriptor<Integer, List<Tuple2<Integer, String>>> mapStateDesc = new MapStateDescriptor<>(
                                     "items", Types.INT, new ListTypeInfo<>(Types.TUPLE(Types.INT, Types.STRING)));
 
-                    // identical to our ruleStateDescriptor above
-                    private final MapStateDescriptor<Integer, Tuple2<Integer, String>> mapStateDescriptor = new MapStateDescriptor<>(
+                    private final MapStateDescriptor<Integer, Tuple2<Integer, String>> broadCastState = new MapStateDescriptor<>(
                             "BroadCastState", Types.INT, TypeInformation.of(new TypeHint<Tuple2<Integer, String>>() {}));
 
                     @Override
                     public void processElement(Tuple2<Integer, String> value, ReadOnlyContext ctx,
                                                Collector<String> out) throws Exception {
-                        final MapState<Integer, List<Tuple2<Integer, String>>> mapState = getRuntimeContext().getMapState(mapStateDesc);
+                        MapState<Integer, List<Tuple2<Integer, String>>> mapState = getRuntimeContext().getMapState(mapStateDesc);
                         Integer id = value.f0;
-                        for (Map.Entry<Integer, Tuple2<Integer, String>> entry : ctx.getBroadcastState(mapStateDescriptor).immutableEntries()) {
+                        for (Map.Entry<Integer, Tuple2<Integer, String>> entry : ctx.getBroadcastState(broadCastState).immutableEntries()) {
                             Integer broadcastId = entry.getKey();
                             Tuple2<Integer, String> broadcastValue = entry.getValue();
 
-                            List<Tuple2<Integer, String>> stateTuple2 = mapState.get(broadcastId);
-                            if (stateTuple2 == null) {
-                                stateTuple2 = new ArrayList<>();
+                            List<Tuple2<Integer, String>> stateTupleList = mapState.get(broadcastId);
+                            if (stateTupleList == null) {
+                                stateTupleList = new ArrayList<>();
                             }
-
-                            if (id.equals(broadcastValue.f0) && !stateTuple2.isEmpty()) {
-                                for (Tuple2<Integer, String> tuple2 : stateTuple2) {
+                            // System.out.println(value + " -- " + broadcastValue + " -- ");
+                            // stateTupleList.forEach(System.out::println);
+                            if (id.equals(broadcastValue.f0) && !stateTupleList.isEmpty()) {
+                                for (Tuple2<Integer, String> tuple2 : stateTupleList) {
                                     out.collect("MATCH：" + tuple2 + "-" + value);
                                 }
-                                stateTuple2.clear();
+                                stateTupleList.clear();
                             }
 
                             if (id.equals(broadcastId)) {
-                                stateTuple2.add(value);
+                                stateTupleList.add(value);
                             }
-                            if (stateTuple2.isEmpty()) {
+                            if (stateTupleList.isEmpty()) {
                                 mapState.remove(broadcastId);
                             } else {
-                                mapState.put(broadcastId, stateTuple2);
+                                mapState.put(broadcastId, stateTupleList);
                             }
-
                         }
                     }
 
                     @Override
                     public void processBroadcastElement(Tuple2<Integer, String> value, Context ctx,
                                                         Collector<String> out) throws Exception {
-                        ctx.getBroadcastState(mapStateDescriptor).put(value.f0, value);
+                        ctx.getBroadcastState(broadCastState).put(value.f0, value);
                     }
                 })
                 .print();
---
+++ flink/src/main/java/com/lu/flink/state/broadcaststatepattern/broad_cast_state.sh
@@ -0,0 +1,15 @@
+nc -l 9999
+1,a
+1,a
+2,b
+2,b
+3,c
+3,c
+
+nc -l 9998
+1,zhangsan
+2,zhangsan
+
+
+#6> MATCH：(1,a)-(1,a)
+#6> MATCH：(2,b)-(2,b)
