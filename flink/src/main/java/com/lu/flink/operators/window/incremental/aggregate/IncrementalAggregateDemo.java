b72c634805a7afa76206b3d0efdda459868567db
luckypig <980534016@qq.com>
2020-10-21 19:58:21 +0800

    :truck: IncrementalAggregateDemo move inner class

--- flink/src/main/java/com/lu/flink/operators/window/incremental/aggregate/IncrementalAggregateDemo.java
+++ flink/src/main/java/com/lu/flink/operators/window/incremental/aggregate/IncrementalAggregateDemo.java
@@ -1,5 +1,6 @@
 package com.lu.flink.operators.window.incremental.aggregate;
 
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -35,5 +36,28 @@ public class IncrementalAggregateDemo {
                 .print();
 
         environment.execute();
+    }
+
+    public static class AverageAggregate implements AggregateFunction<Tuple2<String, Long>, Tuple2<Long, Long>, Double> {
+        @Override
+        public Tuple2<Long, Long> createAccumulator() {
+            return Tuple2.of(0L, 0L);
+
+        }
+
+        @Override
+        public Tuple2<Long, Long> add(Tuple2<String, Long> value, Tuple2<Long, Long> accumulator) {
+            return Tuple2.of(accumulator.f0 + value.f1, accumulator.f1 + 1L);
+        }
+
+        @Override
+        public Double getResult(Tuple2<Long, Long> accumulator) {
+            return ((double) accumulator.f0) / accumulator.f1;
+        }
+
+        @Override
+        public Tuple2<Long, Long> merge(Tuple2<Long, Long> a, Tuple2<Long, Long> b) {
+            return Tuple2.of(a.f0 + b.f0, a.f1 + b.f1);
+        }
     }
 }
--- flink/src/main/java/com/lu/flink/operators/window/incremental/reduce/IncrementalReduceDemo.java
+++ flink/src/main/java/com/lu/flink/operators/window/incremental/reduce/IncrementalReduceDemo.java
@@ -1,10 +1,16 @@
 package com.lu.flink.operators.window.incremental.reduce;
 
+import com.google.common.collect.Lists;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.util.List;
 
 public class IncrementalReduceDemo {
     public static void main(String[] args) throws Exception {
@@ -22,5 +28,22 @@ public class IncrementalReduceDemo {
                 .print();
 
         environment.execute();
+    }
+
+    public static class MyProcessWindowFunction extends ProcessWindowFunction<Tuple3<String, Long, Integer>, Tuple3<String, Long, Tuple3<String, Long, Integer>>, String, TimeWindow> {
+        @Override
+        public void process(String key,
+                            Context context,
+                            Iterable<Tuple3<String, Long, Integer>> elements,
+                            Collector<Tuple3<String, Long, Tuple3<String, Long, Integer>>> out) throws Exception {
+            Tuple3<String, Long, Integer> tuple3 = null;
+            List<Tuple3<String, Long, Integer>> list = Lists.newArrayList(elements);
+            System.out.println(list.size());
+            for (Tuple3<String, Long, Integer> element : list) {
+                tuple3 = element;
+                System.out.println(context.window().toString() + ", " + element);
+            }
+            out.collect(Tuple3.of(key, context.window().getEnd(), tuple3));
+        }
     }
 }
