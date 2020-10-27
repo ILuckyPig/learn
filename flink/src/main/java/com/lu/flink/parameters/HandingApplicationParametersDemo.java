84d62bc9337798b50e5eeef9ad8fa377a8c943da
luckypig <980534016@qq.com>
2020-10-27 20:38:34 +0800

    add HandingApplicationParametersDemo

---
+++ flink/src/main/java/com/lu/flink/parameters/HandingApplicationParametersDemo.java
@@ -0,0 +1,44 @@
+package com.lu.flink.parameters;
+
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class HandingApplicationParametersDemo {
+    public static void main(String[] args) throws Exception {
+        ParameterTool argsParameter = ParameterTool.fromArgs(args);
+        String input = argsParameter.getRequired("input");
+        ParameterTool propertiesParameter = ParameterTool.fromPropertiesFile(HandingApplicationParametersDemo.class
+                .getResource("/log4j.properties").getFile());
+        String root = propertiesParameter.get("log4j.rootLogger");
+        long log = propertiesParameter.getLong("log", 1L);
+        ParameterTool systemProperties = ParameterTool.fromSystemProperties();
+        System.out.println(input);
+        System.out.println(root);
+        System.out.println(log);
+        System.out.println(systemProperties.getRequired("prefix"));
+
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        environment.getConfig().setGlobalJobParameters(systemProperties);
+        environment
+                .fromElements(1, 2, 3)
+                .map(new RichMapFunction<Integer, String>() {
+                    private ParameterTool parameterTool;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        parameterTool = (ParameterTool) getRuntimeContext()
+                                .getExecutionConfig().getGlobalJobParameters();
+                    }
+
+                    @Override
+                    public String map(Integer value) throws Exception {
+                        String prefix = parameterTool.getRequired("prefix");
+                        return prefix + "-" + value;
+                    }
+                })
+                .print();
+        environment.execute();
+    }
+}
