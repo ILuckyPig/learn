7c464a366b149aa9a163896076d314088aef96aa
luckypig <980534016@qq.com>
2020-12-03 20:05:21 +0800

    add RegularJoinDemo

---
+++ flink/src/main/java/com/lu/flink/table/join/RegularJoinDemo.java
@@ -0,0 +1,59 @@
+package com.lu.flink.table.join;
+
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+/**
+ * EvenTime不能用于regular join
+ */
+public class RegularJoinDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        // environment.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        environment.enableCheckpointing(1000);
+        environment.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        environment.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.DELETE_ON_CANCELLATION);
+        environment.setParallelism(2);
+        EnvironmentSettings settings = EnvironmentSettings.newInstance().inStreamingMode().useBlinkPlanner().build();
+        StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(environment, settings);
+
+        tableEnvironment.executeSql(
+                "CREATE TABLE orders (" +
+                        "   order_time TIMESTAMP(3)," +
+                        "   amount INT," +
+                        "   currency STRING" +
+                        // "   WATERMARK FOR order_time AS order_time" +
+                        ") WITH (" +
+                        "   'connector' = 'kafka'," +
+                        "   'topic' = 'Orders'," +
+                        "   'properties.bootstrap.servers' = '127.0.0.1:9092'," +
+                        "   'properties.group.id' = 'kafka-table-demo-group'," +
+                        "   'format' = 'csv'," +
+                        "   'csv.field-delimiter' = ','," +
+                        "   'scan.startup.mode' = 'earliest-offset'" +
+                        ")");
+        tableEnvironment.executeSql(
+                "CREATE TABLE rates_history (" +
+                        "   history_time TIMESTAMP(3)," +
+                        "   currency STRING," +
+                        "   rate INT" +
+                        // "   WATERMARK FOR history_time AS history_time" +
+                        ") WITH (" +
+                        "   'connector' = 'kafka'," +
+                        "   'topic' = 'RatesHistory'," +
+                        "   'properties.bootstrap.servers' = '127.0.0.1:9092'," +
+                        "   'properties.group.id' = 'kafka-table-demo-group'," +
+                        "   'format' = 'csv'," +
+                        "   'csv.field-delimiter' = ','," +
+                        "   'scan.startup.mode' = 'earliest-offset'" +
+                        ")");
+        Table table = tableEnvironment.sqlQuery("SELECT * FROM orders INNER JOIN rates_history ON orders.currency = rates_history.currency");
+        tableEnvironment.toAppendStream(table, Row.class).print();
+        environment.execute();
+    }
+}
