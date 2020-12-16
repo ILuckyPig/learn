1d9320e5bf887a89ebc902cad621120db2dd526b
luanxin <luanxin@social-touch.com>
2020-12-16 22:30:05 +0800

    update event_time_temporal_join.sh

--- producer/src/main/java/com/lu/flink/kafka/producer/table/join/event_time_temporal_join.sh
+++ producer/src/main/java/com/lu/flink/kafka/producer/table/join/event_time_temporal_join.sh
@@ -8,10 +8,10 @@ kafka-console-producer.sh --broker-list kafka_kafka_1:9092 --topic event-time-te
 >4,3,Euro,2020-12-04 10:32:00
 >5,5,Us Dollar,2020-12-04 10:52:00
 
-kafka-console-producer.sh --broker-list kafka_kafka_1:9092 --topic event-time-temporal-join-demo-currency-rates
->Us Dollar,102,2020-12-04 09:00:00
->Euro,114,2020-12-04 09:00:00
->Yen,1,2020-12-04 09:00:00
->Euro,116,2020-12-04 10:45:00
->Euro,119,2020-12-04 11:15:00
->Pounds,108,2020-12-04 11:49:00
+kafka-console-producer.sh --broker-list kafka_kafka_1:9092 --topic event-time-temporal-join-demo-currency-rates --property parse.key=true
+>Us Dollar  Us Dollar,102,2020-12-04 09:00:00
+>Euro Euro,114,2020-12-04 09:00:00
+>Yen  Yen,1,2020-12-04 09:00:00
+>Euro Euro,116,2020-12-04 10:45:00
+>Euro Euro,119,2020-12-04 11:15:00
+>Pounds Pounds,108,2020-12-04 11:49:00
