705b932cbf60005f4b3022ca93a69f1a5a4104ab
luckypig <980534016@qq.com>
2020-12-17 17:25:43 +0800

    add versioned_table_views.sh

---
+++ producer/src/main/java/com/lu/flink/kafka/producer/table/versioned/tables/versioned_table_views.sh
@@ -0,0 +1,7 @@
+kafka-console-producer.sh --broker-list kafka_kafka_1:9092 --topic versioned-table-views-currency-rates
+>{"currency": "Us Dollar", "conversion_rate": 102, "update_time": "2020-12-04 09:00:00"}
+>{"currency": "Euro", "conversion_rate": 114, "update_time": "2020-12-04 09:00:00"}
+>{"currency": "Yen", "conversion_rate": 1, "update_time": "2020-12-04 09:00:00"}
+>{"currency": "Euro", "conversion_rate": 116, "update_time": "2020-12-04 10:45:00"}
+>{"currency": "Euro", "conversion_rate": 119, "update_time": "2020-12-04 11:15:00"}
+>{"currency": "Pounds", "conversion_rate": 108, "update_time": "2020-12-04 11:49:00"}
