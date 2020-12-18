9708f55dae92ce43898604fca63e8fe36e58aa2b
luanxin <luanxin@social-touch.com>
2020-12-18 21:45:25 +0800

    add versioned table view

--- flink/src/main/java/com/lu/flink/table/versioned/tables/VersionedTableViews.java
+++ flink/src/main/java/com/lu/flink/table/versioned/tables/VersionedTableViews.java
@@ -4,6 +4,27 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
+/**
+ * >{"currency": "Us Dollar", "conversion_rate": 102, "update_time": "2020-12-04 09:00:00"}
+ * >{"currency": "Euro", "conversion_rate": 114, "update_time": "2020-12-04 09:00:00"}
+ * >{"currency": "Euro", "conversion_rate": 116, "update_time": "2020-12-04 10:45:00"}
+ * >{"currency": "Yen", "conversion_rate": 1, "update_time": "2020-12-04 09:00:00"}
+ * >{"currency": "Euro", "conversion_rate": 119, "update_time": "2020-12-04 11:15:00"}
+ * >{"currency": "Euro", "conversion_rate": 114, "update_time": "2020-12-04 09:00:00"}
+ * >{"currency": "Yen", "conversion_rate": 100, "update_time": "2020-12-04 09:10:01"}
+ * >{"currency": "Yen", "conversion_rate": 100, "update_time": "2020-12-04 09:01:01"}
+ *
+ * 1> +I(Us Dollar,102.00,2020-12-04T09:00)
+ * 1> +I(Euro,114.00,2020-12-04T09:00)
+ * 1> -U(Euro,114.00,2020-12-04T09:00)
+ * 1> +U(Euro,116.00,2020-12-04T10:45)
+ * 1> +I(Yen,1.00,2020-12-04T09:00)
+ * 1> -U(Euro,116.00,2020-12-04T10:45)
+ * 1> +U(Euro,119.00,2020-12-04T11:15)
+ * 1> -U(Yen,1.00,2020-12-04T09:00)
+ * 1> +U(Yen,100.00,2020-12-04T09:10:01)
+ *
+ */
 public class VersionedTableViews {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
