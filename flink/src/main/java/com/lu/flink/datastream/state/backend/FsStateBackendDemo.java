f19d2669ed296339e95324472326b1317d4288d5
luckypig <980534016@qq.com>
2021-01-15 20:06:56 +0800

    TODO check state backend before throw exception

--- flink/src/main/java/com/lu/flink/datastream/state/backend/FsStateBackendDemo.java
+++ flink/src/main/java/com/lu/flink/datastream/state/backend/FsStateBackendDemo.java
@@ -3,27 +3,37 @@ package com.lu.flink.datastream.state.backend;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.formats.json.JsonNodeDeserializationSchema;
 import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 public class FsStateBackendDemo {
     public static void main(String[] args) throws Exception {
-        // TODO start with checkpoint
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
-        environment.enableCheckpointing(100);
-        String checkpointDirectory = null;
-        if (args.length > 0) {
-            checkpointDirectory = args[0];
-        }
+
+        String checkpointDirectory = parameterTool.get("check", "file:///" + System.getProperty("user.dir") + "/flink/src/main/resources/checkpoints");
         StateBackend stateBackend = new FsStateBackend(checkpointDirectory);
+
+        CheckpointConfig checkpointConfig = environment.getCheckpointConfig();
+        checkpointConfig.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        // checkpointConfig.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.DELETE_ON_CANCELLATION);
+
+        environment.enableCheckpointing(10 * 1000);
+        environment.setParallelism(1);
         environment.setStateBackend(stateBackend);
-        environment.setParallelism(2);
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime upper = now.plusSeconds(3 * 60);
+        LocalDateTime lower = now.plusSeconds(40);
         Properties properties = new Properties();
         properties.put("bootstrap.servers", "localhost:9095");
         properties.put("group.id", "kafka-state-demo-group");
@@ -36,8 +46,14 @@ public class FsStateBackendDemo {
                     @Override
                     public Tuple2<Integer, Integer> reduce(Tuple2<Integer, Integer> value1, Tuple2<Integer, Integer> value2) throws Exception {
                         int o = value1.f1 + value2.f1;
-                        Thread.sleep(100);
-                        return Tuple2.of(value1.f0, o);
+                        Tuple2<Integer, Integer> result = Tuple2.of(value1.f0, o);
+                        LocalDateTime time = LocalDateTime.now();
+                        // TODO 查看是否有状态
+                        if (result.f0 == 2 && time.isBefore(upper)) {
+                            throw new Exception(time + " < " + upper);
+                        }
+                        Thread.sleep(10 * 1000);
+                        return result;
                     }
                 })
                 .print();
