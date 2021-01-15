dd7db17ef76a9c78e867f9495945a77fe3b5f2fc
luckypig <980534016@qq.com>
2021-01-15 20:15:21 +0800

    reset consumer group offset

--- producer/src/main/java/com/lu/flink/kafka/producer/datastream/state/backend/fs_state_backend_demo.sh
+++ producer/src/main/java/com/lu/flink/kafka/producer/datastream/state/backend/fs_state_backend_demo.sh
@@ -1,5 +1,7 @@
 kafka-topics.sh --create --topic fs-state-backend --partitions 2 --zookeeper zookeeper:2181 --replication-factor 1
 kafka-console-producer.sh --broker-list kafka_kafka_1:9092 --topic fs-state-backend
+kafka-consumer-groups.sh --bootstrap-server kafka_kafka_1:9092 --topic fs-state-backend --group kafka-state-demo-group --reset-offsets --to-earliest --execute
+kafka-consumer-groups.sh --bootstrap-server kafka_kafka_1:9092 --describe --group kafka-state-demo-group
 {"id": 1}
 {"id": 2}
 {"id": 1}
