fae2f7b5b4bbc42c2995a7bc92a4192116bf22e4
luckypig <980534016@qq.com>
2022-01-01 21:56:27 +0800

    :bulb: RegularInnerJoinDemo添加注释

--- flink/src/main/java/com/lu/flink/table/join/socket/RegularInnerJoinDemo.java
+++ flink/src/main/java/com/lu/flink/table/join/socket/RegularInnerJoinDemo.java
@@ -3,6 +3,20 @@ package com.lu.flink.table.join.socket;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
+/**
+ * order_log:
+ * <pre>
+ * 1,1,2021-12-25 00:00:00
+ * 2,2,2021-12-25 00:01:00
+ * 3,3,2021-12-25 00:02:00
+ * </pre>
+ * <p>
+ * price_log:
+ * <pre>
+ * 1,10,2021-12-25 00:00:00
+ * 3,40,2021-12-25 00:02:00
+ * </pre>
+ */
 public class RegularInnerJoinDemo {
     public static void main(String[] args) {
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
